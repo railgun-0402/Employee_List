74f35364597b0ac39678305843dd24b9aab886cc
railgun-0402 <april-life.f-441-2011@docomo.ne.jp>
2023-09-21 23:39:50 +0900

    first commit

---
+++ src/main/java/com/example/employeelist/EmployeelistApplication.java
@@ -0,0 +1,13 @@
+package com.example.employeelist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeelistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeelistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/employeelist/EmployeelistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.employeelist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeelistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
